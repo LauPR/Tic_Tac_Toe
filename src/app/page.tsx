33588e38e9eb6dae7d826ab89bfcbc4e4a5eaf42
Laura <126353079+LauPR@users.noreply.github.com>
2026-05-05 17:25:09 -0400

    0.1 C:Button.tsx M:globals.css layout.tsx page.tsx (light/dark mode && generic button styles)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
+import Button from "@/components/ui/Button";
 
 export default function Home() {
 
   return (
-    <div>
+    <div
+      className="
+        bg-bg-primary 
+        h-screen 
+        text-text-primary
+        cursor-default
+      "
+    >
 
-      <main></main>
+      <main className="p-2">
+
+        <h1
+          className="
+            text-6xl 
+            text-center 
+            underline 
+            underline-offset-8
+            decoration-dotted 
+            text-accent-main
+          "
+        >
+          Tic Tac Toe
+        </h1>
+
+        <Button text="large" size={'lg'} variant={'player1'}/>
+        <Button text="medium" size={'md'}/>
+        <Button text="small" size={'sm'} variant={'player2'}/>
+
+      </main>
 
     </div>
   );
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+
+type ButtonProps = {
+    text?: string;
+    variant?: 'primary' | 'player1' | 'player2';
+    size?: 'sm' | 'md' | 'lg';
+    className?: React.ReactNode;
+}
+
+export default function Button({
+    text, variant = 'primary', size = 'md', className
+}: ButtonProps) {
+
+    const variants = {
+        primary: "bg-accent-main      hover:bg-hover-main      focus:bg-focus-main",
+        player1: "bg-accent-primary   hover:bg-hover-primary   focus:bg-focus-primary",
+        player2: "bg-accent-secondary hover:bg-hover-secondary focus:bg-focus-secondary"
+    }
+
+    const sizes = {
+        sm: "text-xl",
+        md: "text-2xl",
+        lg: "text-4xl"
+    }
+
+    return (
+        <button
+            className={cn(
+                "cursor-pointer",
+                "m-1",
+                "p-2",
+                "rounded-2xl",
+                "shadow-sm",
+                "shadow-shadow",
+                "focus:shadow-none",
+                variants[variant],
+                sizes[size],
+                className
+            )}
+        >
+
+            {text}
+
+        </button>
+    )
+}
